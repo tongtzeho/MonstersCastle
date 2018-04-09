a232822a76f208663eab31a04c1982b6883ab002
tongtzeho <tangzihao@pku.edu.cn>
2018-04-09 18:12:51 +0800

    Add files via upload

--- Server/game.py
+++ Server/game.py
@@ -2,7 +2,7 @@
 # Python 2.7.14
 
 import socket, threading, random, struct, time
-import msg, character
+import msg, character, orc
 
 class game(threading.Thread): # run as a game monitor client
 	def __init__(self, username, address, port):
@@ -18,41 +18,72 @@ class game(threading.Thread): # run as a game monitor client
 	def initGame(self):
 		self.gameLock.acquire()
 		self.character = character.character()
+		self.orc = orc.orc()
+		self.gameTime = 0
+		self.level = 0
 		self.gameLock.release()
 	
 	def run(self): # override
-		self.gameTime = time.time()
+		self.gameClock = time.time()
 		while not self.isStop:
 			currTime = time.time()
-			deltaTime = currTime - self.gameTime
-			if deltaTime > 0.0166666666667:
+			deltaTime = currTime - self.gameClock
+			if deltaTime > 0.0166666666667: # 60FPS on server
 				self.update(deltaTime)
-				self.conn.send(msg.encode(self.getGameCurrInfo()))
-				self.gameTime = currTime
+				self.conn.send(msg.encode(self.serialize()))
+				self.gameClock = currTime
 		self.conn.close()
 		
 	def stop(self):
 		self.isStop = True
+	
+	def isAllMonsterDead(self, waitTime, dt):
+		if self.orc.isAlive:
+			self.waitTime = 0
+		else:
+			try:
+				self.waitTime += dt
+			except:
+				self.waitTime = 0
+			if self.waitTime >= waitTime:
+				return True
+		return False
 		
+	def allMonsterReborn(self):
+		self.orc.reborn()
+	
 	def update(self, deltaTime):
 		self.gameLock.acquire()
+		self.gameTime += deltaTime
 		self.character.update(deltaTime)
+		self.orc.update(deltaTime)
+		if (self.level == 0 and self.gameTime >= 5) or (self.level >= 1 and self.level <= 4 and self.isAllMonsterDead(10, deltaTime)):
+			self.allMonsterReborn()
+			self.level += 1
 		self.gameLock.release()
 		
-	def getGameCurrInfo(self):
+	def serialize(self):
 		head = struct.pack("=4s2i16s", "^^^@", self.recog, len(self.username), self.username)
 		self.gameLock.acquire()
 		characterResult = self.character.serialize()
 		characterResult = struct.pack("=h", len(characterResult))+characterResult
+		orcResult = self.orc.serialize()
+		orcResult = struct.pack("=h", len(orcResult))+orcResult
 		self.gameLock.release()
-		return head + characterResult
+		return head + characterResult + orcResult
 		
 	def handle(self, data):
-		characterDataLen = struct.unpack("=h", data[:2])[0]
-		characterData = data[2:2+characterDataLen]
+		offset = 0
+		characterDataLen = struct.unpack("=h", data[offset:offset+2])[0]
+		characterData = data[offset+2:offset+2+characterDataLen]
+		offset += 2+characterDataLen
+		orcDataLen = struct.unpack("=h", data[offset:offset+2])[0]
+		orcData = data[offset+2:offset+2+orcDataLen]
+		offset += 2+orcDataLen
 		self.gameLock.acquire()
 		try:
 			self.character.handle(characterData)
+			self.orc.handle(orcData)
 		except:
 			print ("game.py handle error")
 			pass
---
+++ Server/orc.py
@@ -0,0 +1,68 @@
+# TDFPS ORC (Big Monster)
+# Python 2.7.14
+
+import struct, time
+
+class orc:
+	def __init__(self):
+		self.debug = True
+		self.level = 0
+		self.maxHp = [300, 600, 900, 1200, 1500]
+		self.atk = [100, 200, 300, 400, 500]
+		self.isAlive = 0
+		self.hp = 0
+		self.position = [0.0, 0.0, 0.0]
+		self.rotationY = 0.0
+		self.action = 0 # 0 for born, 1 for walk, 2 for idle, 3 for attack, 4 for die
+		self.rebornPosition = [0.0, 0.0, 50.0]
+		self.rebornRotationY = 180.0
+		self.velocity = [0.0, 0.0, -5.0]
+		self.rebornTime = 1.0
+		self.walkTime = 9.0
+		
+	def reborn(self):
+		if self.level >= len(self.maxHp):
+			return
+		self.isAlive = 1
+		self.hp = self.maxHp[self.level]
+		self.position[0] = self.rebornPosition[0]
+		self.position[1] = self.rebornPosition[1]
+		self.position[2] = self.rebornPosition[2]
+		self.rotationY = self.rebornRotationY
+		self.action = 0
+		self.lifeTime = 0
+		self.level += 1
+		
+	def die(self):
+		self.isAlive = 0
+		self.action = 4
+		
+	def update(self, dt):
+		if self.isAlive:
+			if self.hp <= 0:
+				self.die()
+			else:
+				self.lifeTime += dt
+				if self.lifeTime <= self.rebornTime:
+					self.action = 0
+				elif self.lifeTime <= self.rebornTime + self.walkTime:
+					self.action = 1
+					self.position[0] += self.velocity[0]*dt
+					self.position[1] += self.velocity[1]*dt
+					self.position[2] += self.velocity[2]*dt
+				else:
+					self.action = 2
+		if self.debug:
+			self.log()
+			
+	def handle(self, data):
+		isAlive, level, hp = struct.unpack("=3h", data[:6])
+		if isAlive and level == self.level:
+			self.hp = hp
+			
+	def serialize(self):
+		return struct.pack("=4h4fh", self.isAlive, self.level, self.hp, self.maxHp[self.level-1], self.position[0], self.position[1], self.position[2], self.rotationY, self.action)
+		
+	def log(self):
+		print [self.isAlive, self.level, self.hp], self.position, [self.action]
+				
