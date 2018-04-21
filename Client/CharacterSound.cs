a294622ed867ed02749f20b383bd43b59eddb233
tongtzeho <24854396+tongtzeho@users.noreply.github.com>
2018-04-21 22:48:25 +0800

    Update and rename WalkingSound.cs to CharacterSound.cs

---
+++ Client/CharacterSound.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSound : MonoBehaviour {
+
+	// assigned in editor
+	public AudioSource walk;
+	public AudioSource run;
+	public AudioSource hurt;
+	public AudioSource die;
+	
+	public void SetWalkingSoundState(Control.CharacterState characterState) {
+		switch (characterState) {
+		case Control.CharacterState.Idle:
+			walk.Stop ();
+			run.Stop ();
+			break;
+		case Control.CharacterState.Walk:
+			run.Stop ();
+			if (!walk.isPlaying) {
+				walk.Play ();
+			}
+			break;
+		case Control.CharacterState.Run:
+			walk.Stop ();
+			if (!run.isPlaying) {
+				run.Play ();
+			}
+			break;
+		}
+	}
+
+	public void PlayHurtSound() {
+		if (!hurt.isPlaying) {
+			hurt.Play ();
+		}
+	}
+
+	public void PlayDieSound() {
+		if (!die.isPlaying) {
+			die.Play ();
+		}
+	}
+}
